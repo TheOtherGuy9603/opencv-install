f157c558b01518c23cd59ff24fce0bd0269b4e34
TheOtherGuy <aniket.pande03@gmail.com>
2018-04-02 23:55:56 +0530

    Add files via upload

--- opencv_install.sh
+++ opencv_install.sh
@@ -1,18 +1,18 @@
 sudo apt-get remove x264 libx264-dev
- 
+
 sudo apt-get install build-essential checkinstall cmake pkg-config yasm git gfortran libjpeg8-dev libjasper-dev libpng12-dev libtiff5-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine2-dev libv4l-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev qt5-default libgtk2.0-dev libtbb-dev libatlas-base-dev libfaac-dev libmp3lame-dev libtheora-dev libvorbis-dev libxvidcore-dev libopencore-amrnb-dev libopencore-amrwb-dev x264 v4l-utils libprotobuf-dev protobuf-compiler libgoogle-glog-dev libgflags-dev libgphoto2-dev libeigen3-dev libhdf5-dev doxygen python-dev python-pip python3-dev python3-pip ffmpeg
 
 sudo -H pip2 install -U pip numpy
 sudo -H pip3 install -U pip numpy
 
 git clone https://github.com/opencv/opencv.git
-cd opencv 
-git checkout 3.3.1 
+cd opencv
+git checkout 3.4.1
 cd ..
 
 git clone https://github.com/opencv/opencv_contrib.git
 cd opencv_contrib
-git checkout 3.3.1
+git checkout 3.4.1
 cd ..
 
 cd opencv
