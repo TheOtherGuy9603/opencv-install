b5545ac36dafb4c0eb3412879ed99c11f70045f4
TheOtherGuy <aniket.pande03@gmail.com>
2018-04-03 17:39:12 +0530

    Add files via upload

--- opencv_install.sh
+++ opencv_install.sh
@@ -1,3 +1,5 @@
+cd #!/bin/bash
+cd /home/aniket
 sudo apt-get remove x264 libx264-dev
 
 sudo apt-get install build-essential checkinstall cmake pkg-config yasm git gfortran libjpeg8-dev libjasper-dev libpng12-dev libtiff5-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine2-dev libv4l-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev qt5-default libgtk2.0-dev libtbb-dev libatlas-base-dev libfaac-dev libmp3lame-dev libtheora-dev libvorbis-dev libxvidcore-dev libopencore-amrnb-dev libopencore-amrwb-dev x264 v4l-utils libprotobuf-dev protobuf-compiler libgoogle-glog-dev libgflags-dev libgphoto2-dev libeigen3-dev libhdf5-dev doxygen python-dev python-pip python3-dev python3-pip ffmpeg
@@ -19,7 +21,9 @@ cd opencv
 mkdir build
 cd build
 
-cmake -D WITH_TBB=ON -D WITH_OPENMP=ON -D WITH_IPP=ON -D CMAKE_BUILD_TYPE=RELEASE -D BUILD_EXAMPLES=OFF -D WITH_NVCUVID=ON -D WITH_CUDA=ON -D BUILD_DOCS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_TESTS=OFF -D WITH_CSTRIPES=ON -D WITH_OPENCL=ON CMAKE_INSTALL_PREFIX=/usr/local/ ..
+PYTHON_LOC = $(which python)
+
+cmake -D WITH_TBB=ON -D WITH_OPENMP=ON -D WITH_IPP=ON -D CMAKE_BUILD_TYPE=RELEASE -D BUILD_EXAMPLES=OFF -D WITH_NVCUVID=ON -D WITH_CUDA=ON -D BUILD_DOCS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_TESTS=OFF -D WITH_CSTRIPES=ON -D PYTHON_EXECUTABLE=$PYTHON_LOC -D WITH_OPENCL=ON CMAKE_INSTALL_PREFIX=/usr/local/ ..
 
 make -j4
 sudo make install
@@ -27,3 +31,5 @@ sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
 
 find /usr/local/lib/ -type f -name "cv2*.so"
+
+shutdown
