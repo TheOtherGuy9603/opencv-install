4dba779934f7206b28581cc7dc721b67916a7b15
TheOtherGuy <aniket.pande03@gmail.com>
2018-04-03 17:39:12 +0530

    Add files via upload

--- opencv_install.sh
+++ opencv_install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt-get remove x264 libx264-dev
 
 sudo apt-get install build-essential checkinstall cmake pkg-config yasm git gfortran libjpeg8-dev libjasper-dev libpng12-dev libtiff5-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine2-dev libv4l-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev qt5-default libgtk2.0-dev libtbb-dev libatlas-base-dev libfaac-dev libmp3lame-dev libtheora-dev libvorbis-dev libxvidcore-dev libopencore-amrnb-dev libopencore-amrwb-dev x264 v4l-utils libprotobuf-dev protobuf-compiler libgoogle-glog-dev libgflags-dev libgphoto2-dev libeigen3-dev libhdf5-dev doxygen python-dev python-pip python3-dev python3-pip ffmpeg
@@ -5,12 +7,12 @@ sudo apt-get install build-essential checkinstall cmake pkg-config yasm git gfor
 sudo -H pip2 install -U pip numpy
 sudo -H pip3 install -U pip numpy
 
-git clone https://github.com/opencv/opencv.git
+#git clone https://github.com/opencv/opencv.git
 cd opencv
 git checkout 3.4.1
 cd ..
 
-git clone https://github.com/opencv/opencv_contrib.git
+#git clone https://github.com/opencv/opencv_contrib.git
 cd opencv_contrib
 git checkout 3.4.1
 cd ..
@@ -19,7 +21,9 @@ cd opencv
 mkdir build
 cd build
 
-cmake -D WITH_TBB=ON -D WITH_OPENMP=ON -D WITH_IPP=ON -D CMAKE_BUILD_TYPE=RELEASE -D BUILD_EXAMPLES=OFF -D WITH_NVCUVID=ON -D WITH_CUDA=ON -D BUILD_DOCS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_TESTS=OFF -D WITH_CSTRIPES=ON -D WITH_OPENCL=ON CMAKE_INSTALL_PREFIX=/usr/local/ ..
+PYTHON_LOC = $(which python)
+
+cmake -D WITH_TBB=ON -D WITH_OPENMP=ON -D WITH_IPP=ON -D CMAKE_BUILD_TYPE=RELEASE -D BUILD_EXAMPLES=OFF -D WITH_NVCUVID=ON -D WITH_CUDA=ON -D BUILD_DOCS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_TESTS=OFF -D WITH_CSTRIPES=ON -D PYTHON_EXECUTABLE=$PYTHON_LOC -D WITH_OPENCL=ON CMAKE_INSTALL_PREFIX=/usr/local/ ..
 
 make -j4
 sudo make install
